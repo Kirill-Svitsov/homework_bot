7d868ab62200426fc89f8e95de67af5fe3c49381
Kirill Svitsov <blackaquitance@gmail.com>
2023-05-01 15:25:01 +0100

    Second review version

--- homework.py
+++ homework.py
@@ -23,7 +23,7 @@ tokens = [
     'TELEGRAM_TOKEN',
     'TELEGRAM_CHAT_ID',
 ]
-RETRY_PERIOD = 20
+RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
